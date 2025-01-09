00b24588e77ff8fe3983870f434b8aff77025b4f
psone_phyo <psone9921@gmail.com>
2025-01-09 20:07:29 +0630

    changed baseURL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://memo-craft-api.vercel.app/',
     // timeout: 1000,
     header:{
         'Content-Type': 'application/json'
